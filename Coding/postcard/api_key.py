3b7ed62f3f830f216d1fe886a75791c49fbf0106
Ting Chun Liu <8765498@gmail.com>
2022-11-15 20:03:39 +0100

    update documents ignore

---
+++ Coding/postcard/api_key.py
@@ -0,0 +1,13 @@
+def clarify_apikey():
+    return '8089a12ad3f9411088ffa918fb5ad13c'
+
+def clarify_APPLICATION_ID():
+    return 'imageR'
+
+def flickr_api_key():
+    return '6cf883ddfa7a13989b4b3d82a0215b8f'
+def flickr_api_secret():
+    return '454f8ca3b54b8ca9'
+
+def openai_key():
+    return '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'
